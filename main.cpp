069d38a4d02e49bbf670f6f1a41877a72ee62226
cindystudent <cdy6mao@gmail.com>
2020-05-17 17:55:06 -0700

    basic file io operations

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <gmock/gmock.h>
+#include <fstream>
 using namespace testing;
 using namespace std;
 
 std::string helloWorld (){
-	return "hello world";
+	ofstream myfile;
+	myfile.open("example.txt");
+	string s = "Writing this to a file.";
+	string t = "Hello there!";
+	myfile << s << endl;
+	myfile << t << endl;
+
+	myfile.close();
+	return s + t;
 }
 /*int main(int argc, const char * argv []){
 	std::cout << helloWorld() <<std::endl;
 	return 0;
 }*/
 
-TEST( BlobParserBasics, Vendor )
+TEST( BlobParserBasics, test1 )
 {
-    EXPECT_THAT(helloWorld(), Eq("hello world"));
+	ifstream infile;
+	infile.open("example.txt");
+	string s = "";
+
+	while (!infile.eof()) // To get you all the lines.
+	{
+		string curr;
+		getline(infile, curr); // Saves the line in STRING.
+		cout << "curr is " + curr << endl; // Prints our STRING.
+		s += curr;
+		cout << "s is" + s << endl;
+	}
+    EXPECT_THAT(helloWorld(), Eq(s));
+}
+
+TEST(BlobParserBasics, test2)
+{
+	EXPECT_THAT(1+1, 2);
 }
